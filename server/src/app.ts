6c33b05225330a7d8b8839da6ea3cb44d7721acf
Matthew Rackley <matthew.rackley17@gmail.com>
2023-10-11 00:57:10 -0500

    added db data

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import pool from './db';
 const app = express();
 const port = 3001;
 
@@ -9,4 +9,13 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+});
+
+app.get('/todos', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM todos');
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
